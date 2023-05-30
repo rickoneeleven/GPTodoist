e46818b127bacced4b7391c8cc5697eb9771c93a
Ryan Alexander Partington <pinescore@outlook.com>
2023-05-30 08:52:53 +0000

    changed prompt a little bit

--- helper_gpt.py
+++ helper_gpt.py
@@ -7,7 +7,7 @@ from rich import print
 def create_task_id_prompt(user_message):
     # Remove any 4-digit number at the end and everything after it
     user_message = re.sub(r"\b\d{4}.*$", "", user_message).strip()
-    prompt = f"{user_message}. What was the task ID of that task? Respond with [Task ID: X], in that format exactly, including the square brackets. never fabricate a task id, and also ask me about when i've eaten today. i'm trying to lose weight and could do with your help."
+    prompt = f"{user_message}. What was the task ID of that task? Respond with [Task ID: X], in that format exactly, including the square brackets. never fabricate a task id. Then lets continue our conversation about my eating and weight. If there is no history about it, ask me how i'm getting on generally."
     return prompt
 
 
