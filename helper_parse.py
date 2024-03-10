c9a1d4802bfd02aa9f2c0dbcd9e851fbbec6210f
Ryan Alexander Partington <pinescore@outlook.com>
2024-03-10 14:57:58 +0000

    moved code to helper from main - user_input

--- helper_parse.py
+++ helper_parse.py
@@ -2,6 +2,26 @@ import re
 import module_call_counter
 
 
+def get_user_input():
+    print("You: ", end="")
+    user_input = ""
+    while True:
+        line = input()
+        if line == "ignore":
+            # Ignore everything above
+            user_input = ""
+        elif line == "!!":
+            # Submit everything above and ignore "!!"
+            break
+        elif line.endswith("qq"):  # User input ended
+            user_input += line[:-2]  # Add the current line without the trailing "qq"
+            break
+        else:
+            user_input += line + "\n"  # Add the current line to user_input
+    user_input = user_input.rstrip("\n")
+    return user_input
+
+
 def get_taskname_time_day_as_tuple(
     user_message,
 ):  # takes two messages before parse, i.e add task, move task
